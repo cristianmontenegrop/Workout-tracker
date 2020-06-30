14c2cb09ff9c2a2771fc608f400969ccbe931431
Cris M <cristian.montenegro.p@gmail.com>
2020-06-29 21:38:17 -0700

    html-routes created

---
+++ routes/api-routes.js
@@ -0,0 +1,14 @@
+var db = require("../models");
+
+
+module.exports = function (app) {
+    app.get("/api/workouts", function (req, res) {
+    });
+
+    app.put("api/workouts", function (req, res) {
+    });
+
+    app.get("api/workouts/range", function (req, res) {
+    });
+
+}; 
