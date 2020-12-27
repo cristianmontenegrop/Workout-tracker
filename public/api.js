74e82bdce2b1aa33f12c136e3ec6955955bbb307
Cris M <cristian.montenegro.p@gmail.com>
2020-12-26 22:54:49 -0800

    style(api.js): linted file according to Airbnb style guide

--- public/api.js
+++ public/api.js
@@ -1,48 +1,53 @@
 const API = {
+
   async getLastWorkout() {
     let res;
     try {
-      res = await fetch("/api/workouts");
+      res = await fetch('/api/workouts');
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-    const json = await res.json();
 
+    const json = await res.json();
     return json[json.length - 1];
   },
+
   async addExercise(data) {
-    const id = location.search.split("=")[1];
-    console.log("addExercise() ID: ", id)
-    const res = await fetch("/api/workouts/" + id, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
+    const id = window.location.search.split('=')[1];
+    const apiWorkouts = '/api/workouts/';
+
+    console.log('addExercise() ID: ', id);
+
+    const res = await fetch(apiWorkouts + id, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
     });
 
     const json = await res.json();
-
     return json;
   },
+
   async createWorkout(data = {}) {
-    const res = await fetch("/api/workouts", {
-      method: "POST",
+    const res = await fetch('/api/workouts', {
+      method: 'POST',
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
+      headers: { 'Content-Type': 'application/json' },
     });
 
     const json = await res.json();
-
     return json;
   },
 
   async getWorkoutsInRange() {
+    let res;
     try {
-      const res = await fetch("/api/workouts/range");
+      res = await fetch('/api/workouts/range');
     } catch (err) {
       console.log(err);
     }
-    const json = await res.json();
 
+    const json = await res.json();
     return json;
   },
 };
