904699f6520c6de898aafbe7cb5ca855784ae30d
Cris M <cristian.montenegro.p@gmail.com>
2020-12-26 19:59:49 -0800

    style(api-routes.js): linted file according to Airbnb style guide

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,40 +1,43 @@
-var db = require("../models");
+const db = require('../models');
 
-
-module.exports = function (app) {
-    app.get("/api/workouts", function (req, res) {
-        db.Workout.find({}).then(function (data) {
-            console.log("GET/api/workouts");
-            // console.log(data);
-            // console.log(data[0].exercises);
-            res.json(data);
-        });
+module.exports = (app) => {
+  app.get('/api/workouts', (req, res) => {
+    db.Workout.find({}).then((data) => {
+      console.log('GET/api/workouts');
+      // console.log(data);
+      // console.log(data[0].exercises);
+      res.json(data);
     });
+  });
 
-    app.put("/api/workouts/:id", function (req, res) {
-        console.log("PUT/api/workouts:id");
-        console.log("ID SERVER PUT: ", req.params.id);
-        console.log("REQ.body: ", req.body);
-        db.Workout.updateOne({ _id: req.params.id }, { $push: { exercises: [req.body] }, $inc: { totalDuration: req.body.duration } }).then(function (data) {
-            console.log(data);
-            res.json(data);
-        });
+  app.put('/api/workouts/:id', (req, res) => {
+    console.log('PUT/api/workouts:id');
+    console.log('ID SERVER PUT: ', req.params.id);
+    console.log('REQ.body: ', req.body);
+    db.Workout.updateOne({ _id: req.params.id },
+      {
+        $push: { exercises: [req.body] },
+        $inc: { totalDuration: req.body.duration },
+      })
+      .then((data) => {
+        console.log(data);
+        res.json(data);
+      });
+  });
+
+  app.post('/api/workouts', (req, res) => {
+    db.Workout.create(req).then((data) => {
+      console.log('POST/api/workouts');
+      console.log(data);
+      res.json(data);
     });
+  });
 
-    app.post("/api/workouts", function (req, res) {
-        db.Workout.create(req).then(function (data) {
-            console.log("POST/api/workouts");
-            console.log(data);
-            res.json(data);
-        });
+  app.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({}).then((data) => {
+      console.log('/api/workouts/range');
+      // console.log(data);
+      res.json(data);
     });
-
-    app.get("/api/workouts/range", function (req, res) {
-        db.Workout.find({}).then(function (data) {
-            console.log("/api/workouts/range");
-            // console.log(data);
-            res.json(data);
-        });
-    });
-
-}; 
+  });
+};
