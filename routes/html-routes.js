366c5977b53516b07009a3460ccb49fb68fb63a4
Cris M <cristian.montenegro.p@gmail.com>
2020-12-26 19:46:04 -0800

    style(html-routes.js): linted file according to Airbnb style guide

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,9 @@
-var path = require("path");
+const path = require('path');
 
-module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+module.exports = (app) => {
+  app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
-    app.get("/stats", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
+  app.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '../public/stats.html')));
 
-    app.get("/exercise", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
-
+  app.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '../public/exercise.html')));
 };
